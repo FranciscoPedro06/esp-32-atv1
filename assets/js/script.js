78d51d42b7df6728b237733ccf74b201e67ff452
Francisco Pedro <pedro2006francisco@gmail.com>
2025-09-24 22:22:31 -0300

    Update script.js (Deixando mais intuitivo)

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,16 @@ import {
   onValue,
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
 
+const String firebaseURL = "//Url do seu firebase";
+
 const firebaseConfig = {
-  //seu git config
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://SEU-PROJETO.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SUA_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
